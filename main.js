f82ed77e479317adfe757c0eb4d80aeeb18c0d52
Roberto Morreale <roberto_morreale@hotmail.com>
2023-04-07 13:15:45 +0200

    aggiunta sezione AOS con immagine, da completare effetti JS

--- main.js
+++ main.js
@@ -16,8 +16,11 @@ navIcon.addEventListener('click', ()=>{
 // cattura main navbar
 let mainNavbar = document.querySelector('#mainNavbar');
 
+let myLink = document.querySelectorAll('.myLink');
+
 // cattura loghi
 let logoOrange = document.querySelector('#logoOrange');
+
 let logoWhite = document.querySelector('#logoWhite');
 
 // evento scroll main navbar
@@ -30,6 +33,13 @@ window.addEventListener('scroll', ()=>{
         logoWhite.classList.remove('d-none');
         logoOrange.classList.add('d-none');
 
+        myLink.forEach((el)=>{
+
+            el.classList.add('text-primaryC');
+            el.classList.remove('myLink');
+
+        })        
+
         mainNavbar.style.padding = '20px 0px';
 
     } else{
@@ -37,10 +47,56 @@ window.addEventListener('scroll', ()=>{
         mainNavbar.classList.remove('background-blackC');
         mainNavbar.classList.add('bg-transparent');
         logoOrange.classList.remove('d-none');
-        logoWhite.classList.add('d-none');  
+        logoWhite.classList.add('d-none');
+       
+        myLink.forEach((el)=>{
+
+            el.classList.remove('text-primaryC');
+            el.classList.add('myLink');
+
+        })
         
         mainNavbar.style.padding = '10px 0px';
 
     }
 
 })
+
+
+// CHIAMATA ASINCRONA
+
+// cattura span numero 0
+
+let firstSpan = document.querySelector('#firstSpan');
+
+let secondSpan = document.querySelector('#secondSpan');
+
+let thirdSpan = document.querySelector('#thirdSpan');
+
+// funzione chiamata asincrona
+
+function createInterval(finalNumber, element){
+
+    let counter = 0;
+
+    let interval = setInterval( ()=>{
+
+        if(counter < finalNumber){
+
+            counter++
+
+            element.innerHTML = counter;
+
+        } else {
+
+            clearInterval(interval);
+
+        }
+
+    }, 1)
+
+}
+
+createInterval(1200, firstSpan);
+createInterval(250, secondSpan);
+createInterval(1500, thirdSpan);
